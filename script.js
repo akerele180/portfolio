16f0624fb59395be76eaeebfe6c24ddb058e2103
Akerele, Tosin James <akerele.james1699@gmail.com>
2022-08-25 18:54:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
 function openNav() {
-    var x = document.getElementById("links");
-    var y = document.getElementById("open");
-    var z =document.getElementById("close")
-    if (x.style.display === "block") {
-        x.style.display = "none";
-       
-    } 
-    else {
-        x.style.display = "block";
-        y.style.display = "none";
-        z.style.display = "block";
-    }
+  var x = document.getElementById("links");
+  var y = document.getElementById("open");
+  var z = document.getElementById("close");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+    y.style.display = "none";
+    z.style.display = "block";
+  }
 }
 function closeNav() {
-    var x = document.getElementById("links");
-    var y = document.getElementById("open");
-    var z =document.getElementById("close")
-    if (x.style.display === "block") {
-        x.style.display = "none";
-        z.style.display = "none";
-        y.style.display = "block";
-    } 
+  var x = document.getElementById("links");
+  var y = document.getElementById("open");
+  var z = document.getElementById("close");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+    z.style.display = "none";
+    y.style.display = "block";
+  }
 }
+
+const date = new Date().getFullYear();
+
+const elem = (document.getElementById("attribute").innerHTML = date);
